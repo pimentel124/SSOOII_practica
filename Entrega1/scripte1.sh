43ad55de025cf5192222e4a59d28caeeb78b11c3
Álvaro Pimentel <alvaropimentel124@gmail.com>
2022-05-25 21:14:44 +0200

    up

--- Entrega1/scripte1.sh
+++ Entrega1/scripte1.sh
@@ -1,78 +1,80 @@
 make clean
 make
-echo -e "\x1B[38;2;17;245;120m$ ./mi_mkfs disco 100000\x1b[0m"
+echo "$ rm disco"
+rm disco
+echo "$ ./mi_mkfs disco 100000"
 ./mi_mkfs disco 100000
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$./escribir\x1b[0m"
+echo "$./escribir"
 ./escribir
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./escribir disco "$(cat texto2.txt)" 0"
-echo -e "#escribimos el texto2.txt en los offsets  9.000, 209.000, 30.725.000,"
-echo -e "# 409.605.000 y 480.000.000 de un mismo inodo\x1b[0m"
+echo "$ ./escribir disco "$(cat texto2.txt)" 0"
+echo "#escribimos el texto2.txt en los offsets  9000, 209000, 30725000, "
+echo "#409605000 y 480000000 de un mismo inodo"
 ./escribir disco "$(cat texto2.txt)" 0
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./permitir disco 1 0"
-echo -e "#cambiamos permisos del inodo 1 a 0\x1b[0m"
+echo "$ ./permitir disco 1 0"
+echo "#cambiamos permisos del inodo 1 a 0"
 ./permitir disco 1 0
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer disco 1"
-echo -e "#intentamos leer inodo 1 con permisos=0\x1b[0m"
+echo "$ ./leer disco 1"
+echo "#intentamos leer inodo 1 con permisos=0"
 ./leer disco 1
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./permitir disco 1 6"
-echo -e "#cambiamos permisos del inodo 1 a 6\x1b[0m"
+echo "$ ./permitir disco 1 6"
+echo "#cambiamos permisos del inodo 1 a 6"
 ./permitir disco 1 6
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer disco 1 > ext1.txt\x1b[0m"
+echo "$ ./leer disco 1 > ext1.txt"
 ./leer disco 1 > ext1.txt
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ls -l ext1.txt\x1b[0m"
+echo "$ ls -l ext1.txt"
 ls -l ext1.txt
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./truncar disco 1 409605001\x1b[0m"
+echo "$ ./truncar disco 1 409605001"
 ./truncar disco 1 409605001
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./truncar disco 1 30725003\x1b[0m"
+echo "$ ./truncar disco 1 30725003"
 ./truncar disco 1 30725003
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./truncar disco 1 209008\x1b[0m"
+echo "$ ./truncar disco 1 209008"
 ./truncar disco 1 209008
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./truncar disco 1 9005\x1b[0m"
+echo "$ ./truncar disco 1 9005"
 ./truncar disco 1 9005
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer disco 1 > ext1.txt\x1b[0m"
+echo "$ ./leer disco 1 > ext1.txt"
 ./leer disco 1 > ext1.txt
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ls -l ext1.txt\x1b[0m"
+echo "$ ls -l ext1.txt"
 ls -l ext1.txt
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ cat ext1.txt\x1b[0m"
+echo "$ cat ext1.txt"
 cat ext1.txt
 echo ""
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./truncar disco 1 0\x1b[0m"
+echo "$ ./truncar disco 1 0"
 ./truncar disco 1 0
 echo "################################################################################"
-echo -e "\x1B[38;2;17;245;120m$ ./leer_sf disco\x1b[0m"
+echo "$ ./leer_sf disco"
 ./leer_sf disco
 echo "################################################################################"
 make clean
